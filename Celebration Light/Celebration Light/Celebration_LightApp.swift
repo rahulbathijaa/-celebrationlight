657001a436579a30309b32a69a0631acb22a521f
rahulbathijaa <96887861+rahulbathijaa@users.noreply.github.com>
2021-12-30 14:54:42 -0500

    Created project 😎😬😈👾

---
+++ Celebration Light/Celebration Light/Celebration_LightApp.swift
@@ -0,0 +1,17 @@
+//
+//  Celebration_LightApp.swift
+//  Celebration Light
+//
+//  Created by Rahul Bathija on 2021-12-30.
+//
+
+import SwiftUI
+
+@main
+struct Celebration_LightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

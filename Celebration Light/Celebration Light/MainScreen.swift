5d12aa86724f4279d2f3202e86d69311532da9ca
rahulbathijaa <96887861+rahulbathijaa@users.noreply.github.com>
2022-01-12 20:07:43 -0500

    A few changes to start to the UI build

--- Celebration Light/Celebration Light/MainScreen.swift
+++ Celebration Light/Celebration Light/MainScreen.swift
@@ -1,37 +1,37 @@
-//
-//  MainScreen.swift
-//  Celebration Light
-//
-//  Created by Rahul Bathija on 2022-01-02.
-//
 
-//import SwiftUI
-//
-//struct MainScreen: View {
-//    var body: some View {
-//        VStack {
-//            navigationTitle("Name's Celebration Light")
-//
-//            Image(uiImage: #imageLiteral(resourceName: "Light Mockup (70% Opacity)"))
-//                .resizable()
-//                .frame(width: 200, height: 200)
-//
-//            HStack{
-//
-//                Image(systemName: "gearshape.fill")
-//                    .font(.largeTitle)
-//
-//
-//                Image(systemName: "applelogo")
-//                    .font(.largeTitle)
-//            }
-//
-//        }
-//    }
-//}
-//
-//struct MainScreen_Previews: PreviewProvider {
-//    static var previews: some View {
-//        MainScreen()
-//    }
-//}
+  MainScreen.swift
+  Celebration Light
+
+  Created by Rahul Bathija on 2022-01-02.
+
+
+import SwiftUI
+
+struct MainScreen: View {
+    var body: some View {
+        VStack {
+            navigationTitle("Name's Celebration Light")
+
+            Image(uiImage: #imageLiteral(resourceName: "Light Mockup (70% Opacity)"))
+                .resizable()
+                .frame(width: 200, height: 200)
+
+            HStack{
+
+                Image(systemName: "gearshape.fill")
+                    .font(.largeTitle)
+
+
+                Image(systemName: "applelogo")
+                    .font(.largeTitle)
+            }
+
+        }
+    }
+}
+
+struct MainScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        MainScreen()
+    }
+}
---
+++ Celebration Light/Celebration Light/MainView.swift
@@ -0,0 +1,52 @@
+//
+//  MainView.swift
+//  Celebration Light
+//
+//  Created by Rahul Bathija on 2022-01-12.
+//
+
+import SwiftUI
+
+struct Main: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Main_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
+
+
+//import SwiftUI
+//
+//struct MainScreen: View {
+//    var body: some View {
+//        VStack {
+//            navigationTitle("Name's Celebration Light")
+//
+//            Image(uiImage: #imageLiteral(resourceName: "Light Mockup (70% Opacity)"))
+//                .resizable()
+//                .frame(width: 200, height: 200)
+//
+//            HStack{
+//
+//                Image(systemName: "gearshape.fill")
+//                    .font(.largeTitle)
+//
+//
+//                Image(systemName: "applelogo")
+//                    .font(.largeTitle)
+//            }
+//
+//        }
+//    }
+//}
+//
+//struct MainScreen_Previews: PreviewProvider {
+//    static var previews: some View {
+//        MainScreen()
+//    }
+//}

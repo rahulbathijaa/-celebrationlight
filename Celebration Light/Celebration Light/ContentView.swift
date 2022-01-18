e294971d6aa3e3c8f57abc3c96c75f6ae387f650
rahulbathijaa <96887861+rahulbathijaa@users.noreply.github.com>
2022-01-18 18:23:57 -0500

    Deleting previous onboarding logic; to update with new code

--- Celebration Light/Celebration Light/ContentView.swift
+++ Celebration Light/Celebration Light/ContentView.swift
@@ -101,37 +101,3 @@ struct MainView_Previews: PreviewProvider {
     
     }
 }
-
-class ViewController: UIViewController {
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any aditional setup after loading the view
-    }
-    
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        if Core.shared.isNewUser() {
-            // show onboarding
-            let vc = storyboard?.instantiateViewController(withIdentifier: "welcome") as! WelcomeViewController
-            vc.modalPresentationStyle = .fullScreen
-            present(vc, animated: true)
-        }
-    }
-    
-}
-
-class Core {
-    
-    static let shared = Core()
-    
-    func isNewUser() -> Bool {
-        return !UserDefaults.standard.bool(forKey: "isNewUser")
-    }
-    
-    ///
-    func setIsNotNewUser() {
-        UserDefaults.standard.set(true, forKey: "isNewUser")
-    }
-}
-// sets initial view
--- Celebration Light/Celebration Light/WelcomeViewController.swift
+++ Celebration Light/Celebration Light/WelcomeViewController.swift
@@ -14,8 +14,31 @@ class WelcomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .red
-        // Do any additional setup after loading the view.
+      
+        
+    }
+    
+    private func configure (){
+        // set up scrollview
+        
+        scrollView.frame = holderView.bounds
+        holderView.addSubview(scrollView)
+        
+        let titles = ["Welcome", "Location", "All Set"]
+        for x in 0..<3 {
+            let pageView = UiView(frame: CGRect(x: CGFloat(x) * holderView.frame.size.width, y:0, width: holderView.frame.size.width, height: holderView.frame.size.height))
+            scrollView.addSubview(pageView)
+            
+        //Title, image, button
+        
+            let label = UILabel(frame: CGRect(x:10, y:10, width: pageView.frame.size.width-20, height: 120))
+            let imageView = UiImageView(frame: CGRect(x:10, y: 10+120+10, width: pageView.frame.size.width-20, height: pageView.frame.size.height - 60 - 130 - 15))
+            let button = UIButton(frame: CGRect(x: 10, y: pageView.frame.size.height-60, width: pageView.frame.size.width-20, height: 50))
+            
+            label.textAlignment = .center
+            label.font = UIFont(name: "Helvetica-Bold", size: 32)
+        }
+        
     }
     
     

ab26d0713cdb6965a2b9e8c28dfa33e07236bbdc
rahulbathijaa <96887861+rahulbathijaa@users.noreply.github.com>
2022-01-19 13:31:59 -0500

    Updated onboarding logic for onboarding
    Key items to note here:
    1. Onboarding screen is in content view
    2. Next goal is to get the fav team data input into the onboarding

--- Celebration Light/Celebration Light/ContentView.swift
+++ Celebration Light/Celebration Light/ContentView.swift
@@ -8,6 +8,94 @@
 import SwiftUI
 import UIKit
 
+
+struct OnboardingView: View {
+    @Binding var shouldShowOnboarding: Bool
+  
+    
+    var body: some View{
+        TabView {
+            PageView(
+                title: "Push Notifications",
+                subtitle: "Enable notifications to stay up to date with friends",
+                imageName: "bell",
+                showsDismissButton: false,
+                shouldShowOnboarding: $shouldShowOnboarding
+            )
+                
+            
+            PageView(
+                title: "Bookmarks",
+                subtitle: "Save your favorite pieces of content",
+                imageName: "bookmark",
+                showsDismissButton: false,
+                shouldShowOnboarding: $shouldShowOnboarding
+            )
+            
+            PageView(
+                title: "Flights",
+                subtitle: "Book flights to the places you want to go",
+                imageName: "airplane",
+                showsDismissButton: false,
+                shouldShowOnboarding: $shouldShowOnboarding
+            )
+            
+            PageView(
+                title: "Home",
+                subtitle: "Go home wherever that might be",
+                imageName: "house",
+                showsDismissButton: true,
+                shouldShowOnboarding: $shouldShowOnboarding
+            )
+        }
+        .tabViewStyle(PageTabViewStyle())
+        // Tab view style to show these four pages
+    }
+}
+
+struct PageView: View {
+    let title: String
+    let subtitle: String
+    let imageName: String
+    let showsDismissButton: Bool
+    @Binding var shouldShowOnboarding: Bool
+    
+    var body: some View {
+        VStack {
+            Image(systemName: imageName)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 150, height: 150)
+                .padding()
+            
+            Text(title)
+                .font(.system(size:32))
+                .padding()
+            
+            Text(subtitle)
+                .font(.system(size:24))
+                .multilineTextAlignment(.center)
+                .foregroundColor(Color(.secondaryLabel))
+                .padding()
+            
+            if showsDismissButton {
+                Button(action: {
+                    shouldShowOnboarding.toggle()
+                }, label: {
+                    Text("Get Started")
+                        .bold()
+                        .foregroundColor(Color.white)
+                        .frame(width: 200, height: 50)
+                        .background(Color.green)
+                        .cornerRadius(6)
+                })
+            }
+        }
+    }
+    
+}
+
+
 struct MainView: View {
     
     @State var nameTextField: String = ""
@@ -94,6 +182,10 @@ struct MainView: View {
     // Function to save and append text as code changes
 
 }
+
+// Onboarding
+
+
 
 struct MainView_Previews: PreviewProvider {
     static var previews: some View {

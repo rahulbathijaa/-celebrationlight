715a1c5a59ecbb6a30697d369f0397fa3c6483cb
rahulbathijaa <96887861+rahulbathijaa@users.noreply.github.com>
2022-01-29 15:41:19 -0500

    Background image logic

--- Celebration Light/Celebration Light/Navigation Views/TeamController.swift
+++ Celebration Light/Celebration Light/Navigation Views/TeamController.swift
@@ -9,7 +9,11 @@ import SwiftUI
 
 struct TeamController: View {
     var body: some View {
+        
+        CustomController()
+
         NavigationView{
+                    
         Text("TeamController")
                 .navigationTitle("Select your favorite team")
         }
@@ -17,8 +21,24 @@ struct TeamController: View {
     }
 }
 
+
 struct TeamController_Previews: PreviewProvider {
     static var previews: some View {
         TeamController()
+    }
+}
+
+
+struct CustomController : UIViewControllerRepresentable {
+    
+    func makeUIViewController(context: UIViewControllerRepresentableContext<CustomController>) -> UIViewController {
+        
+        let storyboard = UIStoryboard(name: "Custom", bundle: Bundle.main)
+        let controller = storyboard.instantiateViewController(identifier: "TeamController")
+        return controller
+    }
+    
+    func updateUIViewController( _ uiViewController: UIViewController, context: UIViewControllerRepresentableContext<CustomController>) {
+        
     }
 }
